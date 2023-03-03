bec9548b1da593fe99bd30d72372a9b5fe937ae6
Jaime Alberto Esquivel Acosta <jaimeesquivel@Jaimes-MacBook-Pro.local>
2023-03-02 18:06:58 -0600

    Apollo Server added, Home route added

---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+//import HomeTutor from "../pages/HomeTutor";
+//import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div className="container">
+      { //<Header /> 
+      }
+      { //<HomeTutor /> 
+      }
+    </div>
+  );
+};
+
+export default Home;
